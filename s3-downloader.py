2f06e3da686efae40fe93c96b81c8df25e3e33e3
Bob Sullivan <robertemmettsullivan@gmail.com>
2019-12-15 21:33:57 -0500

    updates to comments and header

--- s3-downloader.py
+++ s3-downloader.py
@@ -1,9 +1,16 @@
 #%%
-''' IMPORTS '''
+''' 
+Filename: s3-downloader.py
+Author: Bob Sullivan @BobSulivn
+Purpose: Recursively download all files in a s3 bucket while preserving bucket
+         structure on target machine.
+Usage: python s3-downloader.py [s3-bucket-name]
+'''
+#  IMPORTS 
 import os
 import sys
 import boto3
-print (os.getcwd())
+
 # bucket name we'd like to download from
 BUCKET_NAME = sys.argv[1]
 
@@ -14,19 +21,19 @@ download_bucket = s3.Bucket(BUCKET_NAME)
 # create object iterator for downloads
 objects = download_bucket.objects.all()
 
-print(objects)
+# loop through each object
 for obj in objects:
+    # get path and filename for each obj in the bucket
     path, filename = os.path.split(obj.key)
-    print(f"path: {path}")
-    print(f"filename: {filename}")
+    # set filepath and filename on target system (client)
     download_name = f"./{path}/{filename}"
-    print(download_name)
     try:
         # try to make directories for files within bucket
         os.makedirs(path)
     except FileExistsError:
         pass
     if filename != "":
+        # download objects that are not folders in s3
         download_bucket.download_file(obj.key, download_name)
 
 # %%
